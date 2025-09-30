460fb7f99d46b66c44a059b9c6d7a8b4accc8213
ambuj vashistha <ambujva123@gmail.com>
2025-09-30 22:50:33 +0530

    skeleton home page

--- mobile-frontend/src/screens/Home.jsx
+++ mobile-frontend/src/screens/Home.jsx
@@ -1,3 +1,15 @@
 import React from "react";
+import { Text, View } from "react-native";
 
-export default function Home() {}
+export default function Home() {
+  return (
+    <View>
+      <View>
+        <Text>View 1</Text>
+      </View>
+      <View>
+        <Text>View 2</Text>
+      </View>
+    </View>
+  );
+}
