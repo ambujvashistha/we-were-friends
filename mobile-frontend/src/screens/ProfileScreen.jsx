2d1906509a68972106089c4cb9a29a7693f9d8d7
ambuj vashistha <ambujva123@gmail.com>
2025-10-01 00:16:45 +0530

    enchancing profile section

--- mobile-frontend/src/screens/ProfileScreen.jsx
+++ mobile-frontend/src/screens/ProfileScreen.jsx
@@ -1,32 +1,40 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function ProfileScreen() {
   return (
     <View>
-      <View>
-        <Image
-          style={{ height: 80, width: 80, borderRadius: 50 }}
-          source={{
-            uri: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
-          }}
-        />
-      </View>
-
-      <View>
+      <View style={styles.profileInfoSection}>
         <View>
-          <Text>Username</Text>
+          <Image
+            style={{ height: 80, width: 80, borderRadius: 50 }}
+            source={{
+              uri: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+            }}
+          />
         </View>
 
         <View>
-          <Text>Followers</Text>
-          <Text>Following</Text>
-          <Text>Memos</Text>
+          <View>
+            <Text>Username</Text>
+          </View>
+
+          <View>
+            <Text>Followers</Text>
+            <Text>Following</Text>
+            <Text>Memos</Text>
+          </View>
         </View>
       </View>
-      <View></View>
-      <View></View>
+
       <Text>Profile</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  profileInfoSection: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "space-evenly",
+  },
+});
