5fe4d82f068ff53d3fa8905c1f3396d527bb7057
ambuj vashistha <ambujva123@gmail.com>
2025-09-30 23:36:14 +0530

    added screen Global Chat to navigations

--- mobile-frontend/src/navigations/AppNavigator.js
+++ mobile-frontend/src/navigations/AppNavigator.js
@@ -2,14 +2,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import SearchScreen from "../screens/SearchScreen";
 import ProfileScreen from "../screens/ProfileScreen";
-
+import GlobalChatScreen from "../screens/GlobalChatScreen";
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigator() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Explore" component={SearchScreen} />
+      <Tab.Screen name="Global Chat" component={GlobalChatScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
