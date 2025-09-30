129ba51d6417cb2b6ff7d35d94d9cb2b3ad388a9
ambuj vashistha <ambujva123@gmail.com>
2025-10-01 01:05:32 +0530

    started explore/search screen

--- mobile-frontend/src/screens/SearchScreen.jsx
+++ mobile-frontend/src/screens/SearchScreen.jsx
@@ -1,14 +1,5 @@
 import React from "react";
 import { View, Text } from "react-native";
 export default function SearchScreen() {
-  return (
-    <View>
-      <View>
-        <Text>View 1</Text>
-      </View>
-      <View>
-        <Text>View 2</Text>
-      </View>
-    </View>
-  );
+  return <View></View>;
 }
