bf565cc0d112ea80ffdffbbb52fd352510973fba
ambuj vashistha <ambujva123@gmail.com>
2025-10-01 00:36:37 +0530

    added text input

--- mobile-frontend/src/screens/GlobalChatScreen.jsx
+++ mobile-frontend/src/screens/GlobalChatScreen.jsx
@@ -1,6 +1,26 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function GlobalChatScreen() {
-  return;
+  const [message, setMessage] = useState("");
+  return (
+    <View>
+      <View></View>
+      <View>
+        <TextInput
+          style={styles.textInput}
+          value={message}
+          onChangeText={() => {}}
+        />
+      </View>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  textInput: {
+    width: 80,
+    height: 30,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+});
