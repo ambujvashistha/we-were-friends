54090f0416538cbfa8851e810fdc723337047c52
ambuj vashistha <ambujva123@gmail.com>
2025-09-30 23:54:44 +0530

    added pfp in profile screen

--- mobile-frontend/src/screens/ProfileScreen.jsx
+++ mobile-frontend/src/screens/ProfileScreen.jsx
@@ -1,6 +1,22 @@
 import React from "react";
-import { Text } from "react-native-gesture-handler";
+import { Text, View, Image } from "react-native";
 
 export default function ProfileScreen() {
-  return <Text>Profile</Text>;
+  return (
+    <View>
+      <View>
+        <Image
+          style={{ height: 80, width: 80, borderRadius: 50 }}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+          }}
+        />
+      </View>
+
+      <View></View>
+      <View></View>
+      <View></View>
+      <Text>Profile</Text>
+    </View>
+  );
 }
