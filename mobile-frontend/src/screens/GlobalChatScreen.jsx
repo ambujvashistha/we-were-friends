217dd3b00a34bb3fdba545fa3ec6fb1c6307bcdd
ambuj vashistha <ambujva123@gmail.com>
2025-10-01 00:25:32 +0530

    started global chat

--- mobile-frontend/src/screens/GlobalChatScreen.jsx
+++ mobile-frontend/src/screens/GlobalChatScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Text } from "react-native-gesture-handler";
+import { Text } from "react-native";
 
 export default function GlobalChatScreen() {
-  return <Text>Global Chat</Text>;
+  return;
 }
