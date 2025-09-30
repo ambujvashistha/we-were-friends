fe5715814a085a3e4808791c93b8f50ac401775d
ambuj vashistha <ambujva123@gmail.com>
2025-09-30 22:58:56 +0530

    made a demo layout for screens

---
+++ mobile-frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FlatList, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <View>
+        <FlatList />
+        <Text>View 1</Text>
+      </View>
+      <View>
+        <Text>View 2</Text>
+      </View>
+    </View>
+  );
+}
--- mobile-frontend/src/screens/SearchScreen.jsx
+++ mobile-frontend/src/screens/SearchScreen.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Text, View } from "react-native";
-
-export default function Home() {
+import { View, Text } from "react-native";
+export default function SearchScreen() {
   return (
     <View>
       <View>
