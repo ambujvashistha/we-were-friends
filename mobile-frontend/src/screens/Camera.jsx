98d9ea5bfa3131832b4252734880ab4bf68525f2
ambuj vashistha <ambujva123@gmail.com>
2025-10-01 17:42:51 +0530

    Camera req working successfully

--- mobile-frontend/src/screens/Camera.jsx
+++ mobile-frontend/src/screens/Camera.jsx
@@ -3,14 +3,15 @@ import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Camera() {
-  const [facing, setFacing] = useState < CameraType > "back";
   const [permission, requestPermission] = useCameraPermissions();
 
   useEffect(() => {
     const askPermission = async () => {
       try {
-        await requestPermission;
-      } catch {}
+        await requestPermission();
+      } catch (err) {
+        console.log(err);
+      }
     };
 
     askPermission();
@@ -30,3 +31,34 @@ export default function Camera() {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 64,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    width: "100%",
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
