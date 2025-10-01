395c502797d59bec0e0430868074904e3e2528ef
ambuj vashistha <ambujva123@gmail.com>
2025-10-01 17:39:54 +0530

    imported Camera.jsx

--- mobile-frontend/src/navigations/AppNavigator.js
+++ mobile-frontend/src/navigations/AppNavigator.js
@@ -3,6 +3,7 @@ import HomeScreen from "../screens/HomeScreen";
 import SearchScreen from "../screens/SearchScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import GlobalChatScreen from "../screens/GlobalChatScreen";
+import Camera from "../screens/Camera";
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigator() {
@@ -10,6 +11,7 @@ export default function AppNavigator() {
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Explore" component={SearchScreen} />
+      <Tab.Screen name="Camera" component={Camera} />
       <Tab.Screen name="Global Chat" component={GlobalChatScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
